7a818e5a5a2500646b79010169db87d8c77e715f
kongil <o_o0122@naver.com>
2022-07-02 19:56:06 +0900

    자바스크립트 인라인  - " " 붙여서 문자열로 인식  - 주석이 아닌 내추럴 템플릿 정상 작동  - 객체 json 타입 변환

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -128,6 +128,15 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+
+        model.addAttribute("user", new User("userA", 10));
+        addUsers(model);
+
+        return "basic/javascript";
+    }
+
     public void addUsers(Model model) {
         ArrayList<User> list = new ArrayList<>();
 
